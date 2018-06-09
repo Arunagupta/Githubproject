a7ce4577b06620d209fa47639f8ade6058643c10
Aruna Kumari <Arunagupta2009@gmil.com>
2018-06-09 11:21:54 +0530

    This is my Ssecond commit

--- MyFirstPOMFramework/MyFirstPOMFramework/src/main/java/POM_Core/Page.java
+++ MyFirstPOMFramework/MyFirstPOMFramework/src/main/java/POM_Core/Page.java
@@ -14,6 +14,8 @@ import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+//import com.relevantcodes.extentreports.ExtentReports;
+//import com.relevantcodes.extentreports.ExtentTest;
 
 
 
